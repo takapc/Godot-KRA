6e5b7f0ff28b7e594e88d7bc15f402f3d10546bb
KITA-SAN-KRA <kairishanxia594@gmail.com>
2023-12-26 14:39:52 +0900

    add horses

--- Main.gd
+++ Main.gd
@@ -2,10 +2,16 @@ extends Node2D
 
 @export var RunningHorse: Array[PackedScene]
 
-
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 func _process(delta):
 	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	$Goal.hide()
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	$Goal.show()
--- horses.gd
+++ horses.gd
@@ -1,7 +1,15 @@
 extends Area2D
+var horse_speed = 0
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 func _ready():
-	$AnimatedSprite2D.animation = "芦毛"
+	show()
+	$AnimatedSprite2D.animation = ""
+	position.y = 0
+	position.x = 216
+
+
+func _on_timer_timeout():
+	position.x += horse_speed
